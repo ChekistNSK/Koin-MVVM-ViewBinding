9c7d2959eb1e4366462d6eaffef489ca600b9a69
ChekistNSK <svadbapro.mobile@gmail.com>
2021-04-08 05:12:08 +0700

    new logic (androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01)

--- app/src/main/java/com/evgenyfedin/flow/MainActivity.kt
+++ app/src/main/java/com/evgenyfedin/flow/MainActivity.kt
@@ -2,12 +2,17 @@ package com.evgenyfedin.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.evgenyfedin.flow.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +23,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        val view = viewBinding.root
-        setContentView(view)
+        setContentView(viewBinding.root)
 
-        viewModel.message.onEach { message ->
-            viewBinding.textView.text = message
-        }.launchThenStarted(lifecycleScope)
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.message.collect { message ->
+                    viewBinding.textView.text = message
+                }
+            }
+        }
 
-        viewModel.messageVisible.onEach { visible ->
-            viewBinding.checkBox.isChecked = visible
-            viewBinding.textView.isVisible = visible
-        }.launchThenStarted(lifecycleScope)
+        lifecycleScope.launch {
+            viewModel.messageVisible.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { visible ->
+                    viewBinding.checkBox.isChecked = visible
+                    viewBinding.textView.isInvisible = !visible
+                }
+        }
 
         viewBinding.checkBox.setOnCheckedChangeListener { _, isChecked ->
             viewModel.setMessageVisible(isChecked)
